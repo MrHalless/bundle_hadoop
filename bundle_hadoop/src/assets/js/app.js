6e8a49b206ae189d0edda07af2802f86892d2556
Vladislav Paskov <v.paskov@arenadata.io>
2021-11-18 12:41:47 +0300

    v.19.1
    Fixed pre code. Add theme atom dark for higlight.js

--- bundle_hadoop/src/assets/js/app.js
+++ bundle_hadoop/src/assets/js/app.js
@@ -277,21 +277,19 @@ $('.block-howto-card__text').on('click', function (event) {
 $('.popup-img').magnificPopup({
   type: 'image',
   mainClass: 'mfp-with-zoom', // this class is for CSS animation below
-
   zoom: {
     enabled: true, // By default it's false, so don't forget to enable it
-
     duration: 300, // duration of the effect, in milliseconds
     easing: 'ease-in-out', // CSS transition easing function
-
-    // The "opener" function should return the element from which popup will be zoomed in
-    // and to which popup will be scaled down
-    // By defailt it looks for an image tag:
     opener: function (openerElement) {
-      // openerElement is the element on which popup was initialized, in this case its <a> tag
-      // you don't need to add "opener" option if this code matches your needs, it's defailt one.
       return openerElement.is('img') ? openerElement : openerElement.find('img');
     }
   }
 
 });
+
+
+$('.hljs').on('click', function () {
+  console.log('test');
+})
+
