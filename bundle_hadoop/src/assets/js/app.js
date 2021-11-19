8953f4e553cff4a6864d0b5d516aa12791f9e446
Vladislav Paskov <v.paskov@arenadata.io>
2021-11-19 15:47:25 +0300

    v.019.3
    add js for close-sm sidebar. (not finished)

--- bundle_hadoop/src/assets/js/app.js
+++ bundle_hadoop/src/assets/js/app.js
@@ -2,46 +2,147 @@ hljs.highlightAll();
 
 // ------------------------------sidebar js-------------------------------------------
 
-
-$('.nav-links_item').on('click', function () {
+$(".nav-links_item").on("click", function () {
   let item = this.parentElement;
-  let needActive = item.querySelector('.is_parent');
-  $(needActive).toggleClass('active');
+  let needActive = item.querySelector(".is_parent");
+  $(needActive).toggleClass("active");
   closeChildren(this);
-})
+});
 
-$('.nav-links_item_parent').on('click', function () {
+$(".nav-links_item_parent").on("click", function () {
   closeChildren(this);
   changeIcon(this);
   let item = this.parentElement;
-  let needActiveLink = item.querySelector('a');
-  $(needActiveLink).toggleClass('active');
-  let needActiveIcon = item.querySelector('.is_parent');
-  $(needActiveIcon).toggleClass('active');
-})
+  let needActiveLink = item.querySelector("a");
+  $(needActiveLink).toggleClass("active");
+  let needActiveIcon = item.querySelector(".is_parent");
+  $(needActiveIcon).toggleClass("active");
+});
 
 function closeChildren(link) {
   let linkElement = link.parentElement;
-  let currentLi = linkElement.querySelector('.is_parent').children;
+  let currentLi = linkElement.querySelector(".is_parent").children;
   for (let i = 0; i < currentLi.length; i++) {
-    let check = currentLi[i].querySelector('.is_parent');
-    if ($(check).hasClass('is_parent')) {
-      $(check).removeClass('active')
-      let deleteActiveStatusIcon = currentLi[i].querySelector('.chevron')
-      $(deleteActiveStatusIcon).removeClass('active')
-      let deleteActiveStatusLink = currentLi[i].querySelector('.nav-links_item_parent').children[0];
-      $(deleteActiveStatusLink).removeClass('active')
+    let check = currentLi[i].querySelector(".is_parent");
+    if ($(check).hasClass("is_parent")) {
+      $(check).removeClass("active");
+      let deleteActiveStatusIcon = currentLi[i].querySelector(".chevron");
+      $(deleteActiveStatusIcon).removeClass("active");
+      let deleteActiveStatusLink = currentLi[i].querySelector(
+        ".nav-links_item_parent"
+      ).children[0];
+      $(deleteActiveStatusLink).removeClass("active");
     }
   }
 }
 
-
-function changeIcon(icon) {
-  let changeIcon = icon.querySelector('.chevron')
-  $(changeIcon).toggleClass('active');
+function closeAllParent() {
+  let allLinks = $(".nav-links_item");
+  for (let i = 0; i < allLinks.length; i++) {
+    closeChildren(allLinks[i]);
+    let item = allLinks[i].parentElement;
+    let needRemoveActive = item.querySelector(".is_parent");
+    $(needRemoveActive).removeClass("active");
+  }
 }
 
+function changeIcon(icon) {
+  let changeIcon = icon.querySelector(".chevron");
+  $(changeIcon).toggleClass("active");
+}
+
+// createnewblocksidebar
+
+function changeContent() {
+  if ($(".sidebar").hasClass("close-sm")) {
+    // console.log("yes");
+    let closeLink = $(".sidebar.close-sm")[0].children[1].children[0].children;
+    for (let i = 0; i < closeLink.length; i++) {
+      $(closeLink[i]).on("click", function (event) {
+        event.stopPropagation();
+        // if ($(".sidebar").hasClass("close-sm")) {
+        $(".nav-links_item_parent").on("click", function (event) {
+          event.stopPropagation();
+
+          let currentLi = this.parentElement.querySelector(
+            ".nav-links_item_parent"
+          );
+          // console.log(currentLi);
+          // console.log($(currentLi.children[0]).attr("href"));
+          let currentLink = $(currentLi.children[0]).attr("href");
+          let currentTitle = currentLi.children[0].innerText;
+          let isParentBlock = this.parentElement.querySelector(".is_parent");
+          // console.log(isParentBlock.parentElement.parentElement);
+          let checkBackTitle = $(isParentBlock.children[0]).hasClass(
+            "backtitle"
+          );
+          if (checkBackTitle) {
+            $(isParentBlock.querySelector(".backtitle")).css("display", "none");
+            if ($(isParentBlock.querySelector(".backtitle")).css("display") == "none" && $(".sidebar").hasClass("close-sm")) {
+
+              $(isParentBlock.querySelector(".backtitle")).css("display", "flex");
+            } else {
+              $(isParentBlock.querySelector(".backtitle")).css("display") == "none"
+            }
+          } else {
+            $(isParentBlock).prepend(`<li class="backtitle">
+                <a href="${currentLink}">${currentTitle}</a>
+                <i class="backicon"></i>
+                </li>`);
+
+            let LinksBackTitle = $('.backtitle');
+            console.log(LinksBackTitle);
+            for (let i = 0; i < LinksBackTitle.length; i++) {
+              $(LinksBackTitle[i].children[0]).on('click', function (event) {
+                event.stopPropagation();
+              });
+            }
+            // $('.backtitle').one('click', function (event) {
+            //   event.stopPropagation();
+            // })
+
+
+          }
+
+          // console.log(this.parentElement.parentElement);
+          // console.log(this.parentElement.children[1]);
+
+          $(this.parentElement.parentElement).css('visibility', 'hidden');
+          $(this.parentElement.children[1]).css('visibility', 'visible');
+
+
+          $(".backtitle").on("click", function (event) {
+            // console.log("test");
+            event.stopPropagation();
+            // console.log(this);
+            let changeItem = this.parentElement.parentElement.querySelector(
+              ".nav-links_item_parent"
+            );
+            $(this.parentElement).removeClass("active");
+            $(changeItem.children[0]).removeClass("active");
+            $(changeItem.children[1]).removeClass("active");
+            $(changeItem.parentElement.parentElement).css('visibility', 'visible')
+
+            // тут должны закрыть текущий блок. и открыть выше стоящий
+          });
+          // console.log('test2');
+        });
+
+        // console.log('test1');
+      });
+
+
+    }
+  } else {
+
+  }
+}
+
+// /createnewblocksidebar
+
 $(".sbar").on("click", function () {
+  closeAllParent();
+
   $("aside").toggleClass("close-sm");
   $(".page_nav").toggleClass("page_nav-sm");
   $(".sbar").toggleClass("sbar-open");
@@ -63,50 +164,39 @@ $(".sbar").on("click", function () {
   $(".howto").attr("data-tooltip", function (index, attr) {
     return attr == "How To!" ? null : "How To!";
   });
-  // console.log($('.conf-par'));
 
-  // $(".sbar").attr("data-tooltip", function (index, attr) {
-  //   return attr == "Open sidebar" ? "Close sidebar" : "Open sidebar";
-  // });
-  // let allUL = document.getElementsByClassName("is_parent");
-  // for (let i = 0; i < allUL.length; i++) {
-  //   allUL[i].classList.remove("active");
-  // }
-  // let navLinksItemParents = document.getElementsByClassName(
-  //   "nav-links_item_parent"
-  // );
-  // for (let i = 0; i < navLinksItemParents.length; i++) {
-  //   let elem = navLinksItemParents[i].children[1];
-  //   elem.parentElement.parentElement
-  //     .querySelector(".is_parent")
-  //     .classList.toggle("active");
-  //   let val = elem.parentElement.querySelectorAll(".chevron");
-  //   for (let i = 0; i < val.length; i++) {
-  //     val[i].parentElement
-  //       .querySelector(".chevron_right")
-  //       .classList.remove("chevron_down");
-  //   }
-  // }  let checkOpenSbar = $(this).hasClass("sbar-open");
   let checkOpenSbar = $(this).hasClass("sbar-open");
   if (!checkOpenSbar) {
+    // console.log("открыт");
     removeIconEvents();
-    $('.conf-par').toggleClass('over-y');
+    $(".conf-par").toggleClass("over-y");
+    // $(".backtitle").css("display", "none");
   } else {
+    // console.log("закрыт");
     addIconEvents();
-    $('.conf-par').toggleClass('over-y');
+    $(".conf-par").toggleClass("over-y");
+    // $(".backtitle").css("display", "flex");
   }
-  // console.log($(this).hasClass("sbar-open"));
-
+  changeContent();
 });
-let checkOpenSbar = $('.sbar').hasClass("sbar-open");
+let checkOpenSbar = $(".sbar").hasClass("sbar-open");
 
 if (!checkOpenSbar) {
+  // console.log("открыт");
   removeIconEvents();
-  $('.conf-par').removeClass('over-y');
+  $(".conf-par").toggleClass("over-y");
 } else {
+  // console.log("закрыт");
   addIconEvents();
-  $('.conf-par').addClass('over-y');
+  $(".conf-par").toggleClass("over-y");
 }
+
+
+
+
+// console.log($('.nav-links_item_parent')[0].children[0].on('click',function(event){
+//   event.stopPropagation();
+// }));
 
 function removeIconEvents() {
   $(".getstarted").css("pointer-events", "none");
@@ -125,9 +215,8 @@ function addIconEvents() {
 }
 // -------------------------------/sidebar js---------------------------------------
 
-
 function copyToClipboard(str) {
-  var area = document.createElement('input');
+  var area = document.createElement("input");
   document.body.appendChild(area);
   area.value = str;
   area.select();
@@ -135,62 +224,67 @@ function copyToClipboard(str) {
   document.body.removeChild(area);
 }
 
-$('.copy-button').on('click', function () {
+$(".copy-button").on("click", function () {
   let code = this.parentElement.children[0].children[0].innerText;
   copyToClipboard(code);
 
   let blockCopied = this.children[1];
-  $(this).css('background-image', 'url("/assets/images/copy-button-green.svg")')
-  $(blockCopied).css('display', 'block')
-})
+  $(this).css(
+    "background-image",
+    'url("/assets/images/copy-button-green.svg")'
+  );
+  $(blockCopied).css("display", "block");
+});
 
-$('.copy-button').on('mouseover', function () {
+$(".copy-button").on("mouseover", function () {
   let blockCopied = this.children[1];
-  $(this).css('background-image', 'url("/assets/images/copy-button-blue.svg")')
-  $(blockCopied).css('display', 'none')
-})
+  $(this).css("background-image", 'url("/assets/images/copy-button-blue.svg")');
+  $(blockCopied).css("display", "none");
+});
 
-$('.copy-button').on('mouseout', function () {
+$(".copy-button").on("mouseout", function () {
   let blockCopied = this.children[1];
-  $(this).css('background-image', 'url("/assets/images/copy-button-grey.svg")')
-  $(blockCopied).css('display', 'none')
-})
+  $(this).css("background-image", 'url("/assets/images/copy-button-grey.svg")');
+  $(blockCopied).css("display", "none");
+});
 
-$('.chevron-lg').on('click', function () {
-  $(this).toggleClass('chevron_down-lg')
-})
+$(".chevron-lg").on("click", function () {
+  $(this).toggleClass("chevron_down-lg");
+});
 
-
-$('.block-howto-card').on('click', function () {
+$(".block-howto-card").on("click", function () {
   console.log($(this.children[0].children[0]));
-  $(this.children[0].children[0]).toggleClass('text-green')
-  $(this).toggleClass('card-open')
-})
+  $(this.children[0].children[0]).toggleClass("text-green");
+  $(this).toggleClass("card-open");
+});
 
-
-$('.block-howto-card__text').on('click', function (event) {
+$(".block-howto-card__text").on("click", function (event) {
   event.stopPropagation();
-})
+});
 
-
-$('.popup-img').magnificPopup({
-  type: 'image',
-  mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+$(".popup-img").magnificPopup({
+  type: "image",
+  mainClass: "mfp-with-zoom", // this class is for CSS animation below
   zoom: {
     enabled: true, // By default it's false, so don't forget to enable it
     duration: 300, // duration of the effect, in milliseconds
-    easing: 'ease-in-out', // CSS transition easing function
+    easing: "ease-in-out", // CSS transition easing function
     opener: function (openerElement) {
-      return openerElement.is('img') ? openerElement : openerElement.find('img');
-    }
-  }
-
+      return openerElement.is("img")
+        ? openerElement
+        : openerElement.find("img");
+    },
+  },
 });
 
-
-$('.hljs').on('click', function () {
-  console.log('test');
-})
+setTimeout(() => {
+  let LinksStopProp = $('.nav-links_item_parent');
+  for (let i = 0; i < LinksStopProp.length; i++) {
+    $(LinksStopProp[i].children[0]).on('click', function (event) {
+      event.stopPropagation();
+    });
+  }
+}, 100);
 
 
 
