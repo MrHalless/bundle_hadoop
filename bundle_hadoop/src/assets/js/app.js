952f901f11e8521f0fc1506d046dad34f0e11fd5
Vladislav Paskov <v.paskov@arenadata.io>
2021-11-25 11:44:21 +0300

    Revert "v.022"
    This reverts commit 893cb5bfb36a14e7ac16d5f5ddcfacfca53b6aa9.

--- bundle_hadoop/src/assets/js/app.js
+++ bundle_hadoop/src/assets/js/app.js
@@ -79,13 +79,11 @@ function changeContent() {
 
     });
 
-    // take 2 children in sidebar.close-sm
-    let closeLink = $(".sidebar.close-sm")[0].children[2].children[0].children;
+    let closeLink = $(".sidebar.close-sm")[0].children[1].children[0].children;
     for (let i = 0; i < closeLink.length; i++) {
       $(closeLink[i]).on("click", function (event) {
-        console.log('test2');
         event.stopPropagation();
-        console.log('test2');
+
 
 
         $(".nav-links_item_parent").on("click", function (event) {
@@ -110,7 +108,6 @@ function changeContent() {
                 $(isParentBlock.querySelector(".backtitle")).css("display") == "none"
               }
             } else {
-              console.log('test');
               $(isParentBlock).prepend(`<li class="backtitle">
                   <a href="${currentLink}">${currentTitle}</a>
                   <i class="backicon"></i>
